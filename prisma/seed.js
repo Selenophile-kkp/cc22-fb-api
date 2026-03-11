286e75416def2659d4d238c0929e15624e3ac9f6
Kwan <kanokwan.kwan00200@gmail.com>
2026-03-11 17:01:01 +0700

    graceful shutdown

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,20 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../src/lib/prisma.js";
 
-const hashedPassword = bcrypt.hashSync("123456", 8);
+const hashedPassword = () => bcrypt.hashSync("123456", 8);
 
 const userData = [
   {
     firstName: "Andy",
     lastName: "Codecamp",
-    password: hashedPassword,
+    password: hashedPassword(),
     email: "andy@ggg.mail",
     profileImage: "https://www.svgrepo.com/show/420364/avatar-male-man.svg",
   },
   {
     firstName: "Bobby",
     lastName: "Codecamp",
-    password: hashedPassword,
+    password: hashedPassword(),
     email: "bobby@ggg.mail",
     profileImage:
       "https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg",
@@ -22,14 +22,14 @@ const userData = [
   {
     firstName: "Candy",
     lastName: "Codecamp",
-    password: hashedPassword,
+    password: hashedPassword(),
     mobile: "1111111111",
     profileImage: "https://www.svgrepo.com/show/420327/avatar-child-girl.svg",
   },
   {
     firstName: "Danny",
     lastName: "Codecamp",
-    password: hashedPassword,
+    password: hashedPassword(),
     mobile: "2222222222",
     profileImage:
       "https://www.svgrepo.com/show/420314/builder-helmet-worker.svg",
--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import app from "./app.js";
-
-dotenv.config();
+import shutdown from "./utils/shutdown.util.js";
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// Listen for termination signals
+process.on("SIGINT", () => shutdown("SIGINT"));
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+
+// Catch unhandled errors
+process.on("uncaughtException", () => shutdown("uncaughtException"));
+process.on("unhandledRejection", () => shutdown("unhandledRejection"));
